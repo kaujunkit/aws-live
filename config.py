94b303ed35dab9e9cbf73399f2b7c86ba6647daa
kaujunkit <130440161+kaujunkit@users.noreply.github.com>
2023-04-11 15:11:31 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cs4hvkymmy2q.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "kaujunkit-employee"
+customregion = "us-east-1"
